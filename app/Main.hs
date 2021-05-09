d6e609e817ecaf12b2ffeedec97de7b62230f9bd
victor1234544 <moskvcihev_v2@gmail.com>
2021-05-09 14:45:13 +0300

    full first and second

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,40 @@
 module Main where
 
-import Lib
-import Prelude hiding (mod, pow, sum, div, and, or, not,sqrt)
+import           Lib
+import           Prelude hiding (and, div, map, mod, not, or, pow, sqrt, sum)
 
 main :: IO ()
 main = print (add 1 1)
 
-zero :: Int -> Int
+zero :: Int -> Int -- const 0
 zero = _N
+
+const_arr :: Int -> [Int] -> Int
+
 
 zero_arr :: [Int] -> Int
 zero_arr _ = zero 0
 
-add :: Int -> Int -> Int
+one_arr :: [Int] -> Int
+one_arr _ = _Z (zero 0)
+
+add :: Int -> Int -> Int -- +
 add a = _R (const a) (\_ past _ -> _Z past) [0]
 
 --1(a)
-mul :: Int -> Int -> Int
+mul :: Int -> Int -> Int -- *
 mul a = _R zero_arr (\_ past _ -> add past a) [0]
 
 --1(b)
-sub1 :: Int -> Int
+sub1 :: Int -> Int --
 sub1 a = _R zero_arr (\i _ _ -> i) [] a
 
 --1(c)
 sub :: Int -> Int -> Int
 sub a b = _R (const a) (\_ past _ -> sub1 past) [0] b
 
---  0 - False
---  0< - True
+--  0 - False -> 0
+--  0< - True -> 1
 
 bool :: Int -> Int
 bool cond = sub cond (sub1 cond)
@@ -77,7 +83,7 @@ multiple :: Int -> Int -> Int
 multiple a b = eq (mod a b) 0
 
 deg :: Int -> Int -> Int
-deg a b = _R (const 2) (\i past xs -> mul past a) [] b
+deg a b = _R (const 1) (\i past xs -> mul past a) [] b
 
 plog :: Int -> Int -> Int
 plog a b = iff (geq a 2) (_R zero_arr (\i past xs -> iff (multiple b (deg a (_Z past))) (_Z past) past) [] b) (undefined)
@@ -91,11 +97,25 @@ sqrt a = _R zero_arr (\_ past _ -> iff (leq (square (_Z past)) a) (_Z past) past
 is_prime :: Int -> Int
 is_prime a = iff (less a 2) 0 (_R (const 1) (\i past _ -> (iff past (iff (less i 2) 1 (not (multiple a i))) 0)) [] (_Z (sqrt a)))
 
---th_prime :: Int -> Int
---th_prime n =
+th_primes :: Int -> Int -> Int
+th_primes i n = iff (is_prime i) (iff (eq n 1) i (th_primes (_Z i) (sub1 n))) (th_primes (_Z i) n)
 
-gedel :: Int -> Int
-gedel n = iff (neq n 0) (_R (const 0) (\i past _ -> iff (multiple n i) (iff (is_prime i) (_Z past) past) past) [] n) (undefined)
+th_prime :: Int -> Int
+th_prime n = th_primes 1 n
+
+gedel_size :: Int -> Int
+gedel_size n = iff (neq n 0) (_R (const 0) (\i past _ -> iff (multiple n i) (iff (is_prime i) (_Z past) past) past) [] n) (undefined)
+
+gedel_th_elem :: Int -> Int -> Int
+gedel_th_elem num n = plog (th_prime n) num
+
+cons :: Int -> Int -> Int
+cons k num = _R (const (deg 2 k)) (\i past _ -> mul past (deg (th_prime (add i 2)) (gedel_th_elem num (_Z i)))) [] (gedel_size num)
+
+--   4 = _Z $ _Z $ _Z $ _Z 0
+-- `(` - 3  `)` - 5
+gedel_check_correct :: Int -> Int
+gedel_check_correct num = iff (eq 1 (_R one_arr (\i past _ -> iff (eq past 0) 0 (sub (add past 4) (gedel_th_elem num (_Z i))) ) [] (_Z (gedel_size num)))) 1 0
 
 ackermann :: Int -> Int -> Int
 ackermann m n= iff (eq m 0) (_Z n) (iff (eq n 0) (ackermann (sub1 m) (_Z 0)) (ackermann (sub1 m) (ackermann m (sub1 n))))
